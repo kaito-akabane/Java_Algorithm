6aaa93d2874ff12436ba54553157bd4a4cec9fc8
AKABANE <kmps0419@naver.com>
2023-10-16 16:44:55 +0900

    231016

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,14 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Deque;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        
-        Deque<Integer> q = new ArrayDeque<>();
-        
         StringTokenizer st = new StringTokenizer(br.readLine());
-        StringTokenizer st2 = new StringTokenizer(br.readLine());
-        int cntQ = 0;
-        for(int i=0;i<n;i++) {
-            if(Integer.parseInt(st.nextToken())==0) {
-                q.addLast(Integer.parseInt(st2.nextToken()));
-                cntQ++;
-            }
-            else st2.nextToken();
-        }
-        
-        StringBuilder sb = new StringBuilder();
-        int m = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
-        for(int i=0;i<m;i++) {
-            q.addFirst(Integer.parseInt(st.nextToken()));
-            sb.append(q.pollLast()).append(" ");
-        }
-        System.out.println(sb);
+        int W = Integer.parseInt(st.nextToken());
+        int H = Integer.parseInt(st.nextToken());
+        System.out.printf("%.1f%n", (double)W*H/2);
     }
 }
